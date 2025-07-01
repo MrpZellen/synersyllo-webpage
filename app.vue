4a6db501ffe238187b1ba74fdc0ca4715603b502
zellency <Ben@benleonard.net>
2025-07-01 16:39:48 -0600

    Start of the new project!

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>  
+<div class="h-full flex flex-grow justify-center items-center space-y-4 space-x-4 flex-col">
+  <h1 class="text-3xl font-bold underline p-3">Welcome to SynerSyllo!</h1>
+  <div class="text-xl p-3">We hope you enjoy the site!</div>
+</div>
+</template>
